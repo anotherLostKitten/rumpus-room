99a627cc33e7c8bfc8d679451ad61d7be89a8ad2
TerryMinal <tguan@stuy.edu>
2017-03-05 18:47:32 -0500

    comments added

--- maize4days/p4_2333_WuY-GuanT/Maze.java
+++ maize4days/p4_2333_WuY-GuanT/Maze.java
@@ -7,17 +7,19 @@
  */
 
 /***
- * SKEELTON for class 
+ * SKEELTON for class
  * MazeSolver
  * Implements a blind depth-first exit-finding algorithm.
  * Displays probing in terminal.
- * 
- * USAGE: 
+ *
+ * USAGE:
  * $ java Maze [mazefile]
  * (mazefile is ASCII representation of maze, using symbols below)
- * 
+ *
  * ALGORITHM for finding exit from starting position:
- *  <INSERT YOUR SUMMARY OF ALGO HERE>
+ The algorithm starts at the starting point and checks if it is on the end point.
+ If it is not on the endpoint, it recurisvely looks in every four directions (up, down, left, right)
+and checks if it is an endpoint until it reaches the end. If it hits a wall it stops looking in that direction
  ***/
 
 //enable file I/O
@@ -25,69 +27,69 @@ import java.io.*;
 import java.util.*;
 
 
-class MazeSolver 
+class MazeSolver
 {
     private char[][] maze;
     private int h, w; //height, width of maze
     private boolean solved;
-    
+
     //initialize constants for map component symbols
     final private char HERO =         '@';
     final private char PATH =         '#';
     final private char WALL =         ' ';
     final private char EXIT =         '$';
     final private char VISITED_PATH = '.';
-    
-    
-    public MazeSolver( String inputFile ) 
+
+
+    public MazeSolver( String inputFile )
     {
-	// init 2D array to represent maze 
+	// init 2D array to represent maze
 	// (80x25 is default terminal window size)
 	maze = new char[80][25];
 	h = 0;
 	w = 0;
-	
+
 	//transcribe maze from file into memory
 	try {
 	    Scanner sc = new Scanner( new File(inputFile) );
-	    
+
 	    System.out.println( "reading in file..." );
-	    
+
 	    int row = 0;
-	    
+
 	    while( sc.hasNext() ) {
-		
+
 		String line = sc.nextLine();
-		
-		if ( w < line.length() ) 
+
+		if ( w < line.length() )
 		    w = line.length();
-		
+
 		for( int i=0; i<line.length(); i++ )
 		    maze[i][row] = line.charAt( i );
-		
+
 		h++;
 		row++;
-	    } 
-	    
+	    }
+
 	    for( int i=0; i<w; i++ )
 		maze[i][row] = WALL;
 	    h++;
 	    row++;
-	    
+
 	} catch( Exception e ) { System.out.println( "Error reading file" ); }
-	
+
 	//at init time, maze has not been solved:
 	solved = false;
     }//end constructor
-    
-    
-    public String toString() 
+
+
+    public String toString()
     {
 	//send ANSI code "ESC[0;0H" to place cursor in upper left
-	String retStr = "[0;0H";  
+	String retStr = "[0;0H";
 	//emacs shortcut: C-q, then press ESC
 	//emacs shortcut: M-x quoted-insert, then press ESC
-	
+
 	int i, j;
 	for( i=0; i<h; i++ ) {
 	    for( j=0; j<w; j++ )
@@ -96,10 +98,10 @@ class MazeSolver
 	}
 	return retStr;
     }
-    
-    
+
+
     //helper method to keep try/catch clutter out of main flow
-    private void delay( int n ) 
+    private void delay( int n )
     {
 	try {
 	    Thread.sleep(n);
@@ -107,17 +109,17 @@ class MazeSolver
 	    System.exit(0);
 	}
     }
-    
-    
+
+
     /*********************************************
      * void solve(int x,int y) -- recursively finds maze exit (depth-first)
      * @param x starting x-coord, measured from left
      * @param y starting y-coord, measured from top
      *********************************************/
     public void solve( int x, int y ) {
-	
+
 	delay(50); //slow it down enough to be followable
-	
+
 	//primary base case
 	if ( x > w || x < 0 || y > h || y < 0 ){
 
@@ -147,9 +149,9 @@ class MazeSolver
 	    System.out.println( this );
 		//}
 	    /*
-	        a 
+	        a
 	      d @ b
-	        c 
+	        c
 	     */
 	    solve(x,y+1); // a
 	    solve(x+1,y); // b
@@ -157,34 +159,34 @@ class MazeSolver
 	    solve(x-1,y); // d
 
 	    maze[x][y] = PATH;
-	    
+
 	}
     }
-    
+
     //accessor method to help with randomized drop-in location
     public boolean onPath( int x, int y) { return maze[x][y] == PATH; }
-    
+
 }//end class MazeSolver
 
 
-public class Maze 
+public class Maze
 {
-    public static void main( String[] args ) 
+    public static void main( String[] args )
     {
 	try {
 	    String mazeInputFile = args[0];
-	    
+
 	    MazeSolver ms = new MazeSolver( mazeInputFile );
 	    //clear screen
-	    System.out.println( "[2J" ); 
-	    
-	    //display maze 
+	    System.out.println( "[2J" );
+
+	    //display maze
 	    System.out.println( ms );
-	    
+
 	    //drop hero into the maze (coords must be on path)
 	    //comment next line out when ready to randomize startpos
-	    ms.solve( 0, 0 ); 
-	    
+	    ms.solve( 0, 0 );
+
 		/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 		//drop our hero into maze at random location on path
 		//the Tim Diep way:
@@ -195,13 +197,13 @@ public class Maze
 		startX = r.nextInt( 80 );
 		startY = r.nextInt( 25 );
 		}
-		
+
 		ms.solve( startX, startY );
 		~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-	} catch( Exception e ) { 
+	} catch( Exception e ) {
 	    System.out.println( "Error reading input file." );
-	    System.out.println( "Usage: java Maze <filename>" ); 
+	    System.out.println( "Usage: java Maze <filename>" );
 	}
     }
-    
+
 }//end class Maze
